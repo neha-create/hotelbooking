872c6033e41ae26e6b80321b870c2fab869920af
neha-create <62049112+neha-create@users.noreply.github.com>
2020-03-16 16:22:21 +0530

    Delete BaseVM.java

--- src/main/java/com/hotelbooking/vm/core/BaseVM.java
+++
@@ -1,9 +0,0 @@
-package com.hotelbooking.vm.core;
-
-import java.io.Serializable;
-
-public class BaseVM implements Serializable {
-
-    private static final long serialVersionUID = 2317345894308426693L;
-
-}
